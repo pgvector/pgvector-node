d8e59dec79b94d0348399d0cf550ab64cd4ed6ec
Andrew Kane <andrew@ankane.org>
2023-05-28 20:53:57 -0700

    Updated types [skip ci]

--- types/drizzle-orm/index.d.ts
+++ types/drizzle-orm/index.d.ts
@@ -5,3 +5,6 @@ export const vector: <TName extends string>(dbName: TName, fieldConfig?: unknown
     notNull: false;
     hasDefault: false;
 }>;
+export function l2Distance(column: any, value: any): import("drizzle-orm/column.d-c31e7ad3").S<any>;
+export function maxInnerProduct(column: any, value: any): import("drizzle-orm/column.d-c31e7ad3").S<any>;
+export function cosineDistance(column: any, value: any): import("drizzle-orm/column.d-c31e7ad3").S<any>;
