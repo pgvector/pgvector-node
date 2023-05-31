d368581999e811ec0c4eeb085ee9a3e02d24c9f9
Andrew Kane <andrew@ankane.org>
2023-05-31 00:36:33 -0700

    Fixed types for drizzle-orm [skip ci]

--- types/drizzle-orm/index.d.ts
+++ types/drizzle-orm/index.d.ts
@@ -1,10 +1,4 @@
-export const vector: <TName extends string>(dbName: TName, fieldConfig?: unknown) => import("drizzle-orm/pg-core").PgCustomColumnBuilder<{
-    name: TName;
-    data: unknown;
-    driverParam: unknown;
-    notNull: false;
-    hasDefault: false;
-}>;
-export function l2Distance(column: any, value: any): import("drizzle-orm/column.d-c31e7ad3").S<any>;
-export function maxInnerProduct(column: any, value: any): import("drizzle-orm/column.d-c31e7ad3").S<any>;
-export function cosineDistance(column: any, value: any): import("drizzle-orm/column.d-c31e7ad3").S<any>;
+export const vector: any;
+export function l2Distance(column: any, value: any): any;
+export function maxInnerProduct(column: any, value: any): any;
+export function cosineDistance(column: any, value: any): any;
