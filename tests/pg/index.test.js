1c69bbfd15b2cbb1008a28f32326359c3af725f8
Andrew Kane <andrew@ankane.org>
2023-12-10 23:47:35 -0500

    Improved test [skip ci]

--- tests/pg/index.test.js
+++ tests/pg/index.test.js
@@ -6,8 +6,8 @@ const client = new Client({database: 'pgvector_node_test'});
 beforeAll(async () => {
   await client.connect();
   await client.query('CREATE EXTENSION IF NOT EXISTS vector');
-  await client.query('DROP TABLE IF EXISTS items');
-  await client.query('CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3))');
+  await client.query('DROP TABLE IF EXISTS pg_items');
+  await client.query('CREATE TABLE pg_items (id bigserial PRIMARY KEY, embedding vector(3))');
   await pgvector.registerType(client);
 });
 
@@ -16,12 +16,12 @@ afterAll(async () => {
 });
 
 beforeEach(async () => {
-  await client.query('DELETE FROM items');
+  await client.query('DELETE FROM pg_items');
 });
 
 test('works', async () => {
-  await client.query('INSERT INTO items (embedding) VALUES ($1)', [pgvector.toSql([1, 2, 3])]);
-  const { rows } = await client.query('SELECT * FROM items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 2, 3])]);
+  await client.query('INSERT INTO pg_items (embedding) VALUES ($1)', [pgvector.toSql([1, 2, 3])]);
+  const { rows } = await client.query('SELECT * FROM pg_items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 2, 3])]);
   expect(rows[0].embedding).toStrictEqual([1, 2, 3]);
 });
 
@@ -36,8 +36,8 @@ test('pool', async () => {
   pool.on('connect', async function (client) {
     await pgvector.registerType(client);
   });
-  await pool.query('INSERT INTO items (embedding) VALUES ($1)', [pgvector.toSql([1, 2, 3])]);
-  const { rows } = await pool.query('SELECT * FROM items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 2, 3])]);
+  await pool.query('INSERT INTO pg_items (embedding) VALUES ($1)', [pgvector.toSql([1, 2, 3])]);
+  const { rows } = await pool.query('SELECT * FROM pg_items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 2, 3])]);
   expect(rows[0].embedding).toStrictEqual([1, 2, 3]);
   await pool.end();
 });
