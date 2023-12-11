67f1b0e774af2587a14dfe7031ede7467ba86436
Andrew Kane <andrew@ankane.org>
2023-12-10 23:47:11 -0500

    Improved test [skip ci]

--- tests/pg-promise/index.test.js
+++ tests/pg-promise/index.test.js
@@ -12,8 +12,8 @@ const db = pgp({database: 'pgvector_node_test'});
 
 beforeAll(async () => {
   await db.none('CREATE EXTENSION IF NOT EXISTS vector');
-  await db.none('DROP TABLE IF EXISTS items');
-  await db.none('CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3))');
+  await db.none('DROP TABLE IF EXISTS pg_promise_items');
+  await db.none('CREATE TABLE pg_promise_items (id bigserial PRIMARY KEY, embedding vector(3))');
 });
 
 afterAll(async () => {
@@ -21,12 +21,12 @@ afterAll(async () => {
 });
 
 beforeEach(async () => {
-  await db.none('DELETE FROM items');
+  await db.none('DELETE FROM pg_promise_items');
 });
 
 test('works', async () => {
-  await db.none('INSERT INTO items (embedding) VALUES ($1)', [pgvector.toSql([1, 2, 3])]);
-  const rows = await db.any('SELECT * FROM items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 2, 3])]);
+  await db.none('INSERT INTO pg_promise_items (embedding) VALUES ($1)', [pgvector.toSql([1, 2, 3])]);
+  const rows = await db.any('SELECT * FROM pg_promise_items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 2, 3])]);
   expect(rows[0].embedding).toStrictEqual([1, 2, 3]);
 });
 
