2d9df8ad95b0c94ed7e7b1ee85520c2251a58cac
Andrew Kane <andrew@ankane.org>
2023-12-11 21:42:44 -0500

    Updated test [skip ci]

--- tests/knex/index.test.js
+++ tests/knex/index.test.js
@@ -29,7 +29,9 @@ test('works', async () => {
     {embedding: pgvector.toSql([1, 1, 2])}
   ];
   await knex('knex_items').insert(newItems);
-  const items = await knex('knex_items').orderByRaw('embedding <-> ?', pgvector.toSql([1, 1, 1])).limit(5);
+  const items = await knex('knex_items')
+    .orderByRaw('embedding <-> ?', pgvector.toSql([1, 1, 1]))
+    .limit(5);
   expect(pgvector.fromSql(items[0].embedding)).toStrictEqual([1, 1, 1]);
   expect(pgvector.fromSql(items[1].embedding)).toStrictEqual([1, 1, 2]);
   expect(pgvector.fromSql(items[2].embedding)).toStrictEqual([2, 2, 2]);
