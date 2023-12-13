4c066eb5e83e354f92d9030f01ce89145b95043d
Andrew Kane <andrew@ankane.org>
2023-12-13 17:41:39 -0500

    Switched example to vector function [skip ci]

--- tests/postgres/index.test.mjs
+++ tests/postgres/index.test.mjs
@@ -1,5 +1,5 @@
 import postgres from 'postgres';
-import pgvector from 'pgvector/utils';
+import { vector } from 'pgvector';
 
 test('example', async () => {
   const sql = postgres({database: 'pgvector_node_test', onnotice: function() {}});
@@ -9,18 +9,18 @@ test('example', async () => {
   await sql`CREATE TABLE postgres_items (id serial PRIMARY KEY, embedding vector(3))`;
 
   const newItems = [
-    {embedding: pgvector.toSql([1, 1, 1])},
-    {embedding: pgvector.toSql([2, 2, 2])},
-    {embedding: pgvector.toSql([1, 1, 2])}
+    {embedding: vector([1, 1, 1])},
+    {embedding: vector([2, 2, 2])},
+    {embedding: vector([1, 1, 2])}
   ];
   await sql`INSERT INTO postgres_items ${ sql(newItems, 'embedding') }`;
 
-  const embedding = pgvector.toSql([1, 1, 1]);
+  const embedding = vector([1, 1, 1]);
   const items = await sql`SELECT * FROM postgres_items ORDER BY embedding <-> ${ embedding } LIMIT 5`;
   expect(items.map(v => v.id)).toStrictEqual([1, 3, 2]);
-  expect(pgvector.fromSql(items[0].embedding)).toStrictEqual([1, 1, 1]);
-  expect(pgvector.fromSql(items[1].embedding)).toStrictEqual([1, 1, 2]);
-  expect(pgvector.fromSql(items[2].embedding)).toStrictEqual([2, 2, 2]);
+  expect(vector(items[0].embedding).toArray()).toStrictEqual([1, 1, 1]);
+  expect(vector(items[1].embedding).toArray()).toStrictEqual([1, 1, 2]);
+  expect(vector(items[2].embedding).toArray()).toStrictEqual([2, 2, 2]);
 
   await sql`CREATE INDEX ON postgres_items USING hnsw (embedding vector_l2_ops)`;
 
