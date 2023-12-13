281adeb26083318653d5c89f7024b4556409cfd9
Andrew Kane <andrew@ankane.org>
2023-12-13 17:43:07 -0500

    Switched example to vector function [skip ci]

--- tests/pg-promise/index.test.mjs
+++ tests/pg-promise/index.test.mjs
@@ -1,10 +1,10 @@
 import pgpromise from 'pg-promise';
-import pgvector from 'pgvector/pg';
+import { vector, registerType } from 'pgvector/pg';
 
 test('example', async () => {
   const initOptions = {
     async connect(e) {
-      await pgvector.registerType(e.client);
+      await registerType(e.client);
     }
   };
   const pgp = pgpromise(initOptions);
@@ -15,13 +15,13 @@ test('example', async () => {
   await db.none('CREATE TABLE pg_promise_items (id serial PRIMARY KEY, embedding vector(3))');
 
   const params = [
-    pgvector.toSql([1, 1, 1]),
-    pgvector.toSql([2, 2, 2]),
-    pgvector.toSql([1, 1, 2])
+    vector([1, 1, 1]),
+    vector([2, 2, 2]),
+    vector([1, 1, 2])
   ];
   await db.none('INSERT INTO pg_promise_items (embedding) VALUES ($1), ($2), ($3)', params);
 
-  const items = await db.any('SELECT * FROM pg_promise_items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 1, 1])]);
+  const items = await db.any('SELECT * FROM pg_promise_items ORDER BY embedding <-> $1 LIMIT 5', [vector([1, 1, 1])]);
   expect(items.map(v => v.id)).toStrictEqual([1, 3, 2]);
   expect(items[0].embedding).toStrictEqual([1, 1, 1]);
   expect(items[1].embedding).toStrictEqual([1, 1, 2]);
