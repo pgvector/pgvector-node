2754f52a63979bd9f1c94b0bdfb91ca9178c93c0
Andrew Kane <andrew@ankane.org>
2023-12-13 18:00:44 -0500

    Improved example [skip ci]

--- tests/pg-promise/index.test.mjs
+++ tests/pg-promise/index.test.mjs
@@ -17,12 +17,13 @@ test('example', async () => {
   const params = [
     vector([1, 1, 1]),
     vector([2, 2, 2]),
-    vector([1, 1, 2])
+    vector([1, 1, 2]),
+    null
   ];
-  await db.none('INSERT INTO pg_promise_items (embedding) VALUES ($1), ($2), ($3)', params);
+  await db.none('INSERT INTO pg_promise_items (embedding) VALUES ($1), ($2), ($3), ($4)', params);
 
   const items = await db.any('SELECT * FROM pg_promise_items ORDER BY embedding <-> $1 LIMIT 5', [vector([1, 1, 1])]);
-  expect(items.map(v => v.id)).toStrictEqual([1, 3, 2]);
+  expect(items.map(v => v.id)).toStrictEqual([1, 3, 2, 4]);
   expect(items[0].embedding).toStrictEqual([1, 1, 1]);
   expect(items[1].embedding).toStrictEqual([1, 1, 2]);
   expect(items[2].embedding).toStrictEqual([2, 2, 2]);
