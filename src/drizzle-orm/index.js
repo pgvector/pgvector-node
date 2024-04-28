00a976fe96ba220e895d94225424d588319e297d
Andrew Kane <andrew@ankane.org>
2024-04-28 10:21:26 -0700

    Removed unused require - #20

--- src/drizzle-orm/index.js
+++ src/drizzle-orm/index.js
@@ -1,6 +1,5 @@
 const { sql } = require('drizzle-orm');
 const { PgColumn, PgColumnBuilder } = require('drizzle-orm/pg-core');
-const util = require('node:util');
 const utils = require('../utils');
 
 class PgVectorBuilder extends PgColumnBuilder {
