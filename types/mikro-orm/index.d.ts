43db1cb0c335f9b2e9884d21e57e851823012e5d
Andrew Kane <andrew@ankane.org>
2024-05-06 23:55:53 -0700

    Updated types [skip ci]

--- types/mikro-orm/index.d.ts
+++ types/mikro-orm/index.d.ts
@@ -1,17 +1,7 @@
-export class VectorType extends Type<string, string> {
-    constructor();
-    convertToDatabaseValue(value: any, platform: any): string;
-    convertToJSValue(value: any, platform: any): any;
-    getColumnType(prop: any, platform: any): any;
-}
-export class HalfvecType extends Type<string, string> {
-    constructor();
-    convertToDatabaseValue(value: any, platform: any): string;
-    convertToJSValue(value: any, platform: any): any;
-    getColumnType(prop: any, platform: any): any;
-}
+import { VectorType } from "./vector";
+import { HalfvecType } from "./halfvec";
 export function l2Distance(column: any, value: any, em: any): any;
 export function maxInnerProduct(column: any, value: any, em: any): any;
 export function cosineDistance(column: any, value: any, em: any): any;
 export function l1Distance(column: any, value: any, em: any): any;
-import { Type } from "@mikro-orm/core/types";
+export { VectorType, HalfvecType };
--- types/utils/index.d.ts
+++ types/utils/index.d.ts
@@ -3,4 +3,5 @@ export function toSql(value: any): string;
 export function sqlType(dimensions: any): any;
 export function vectorType(dimensions: any): any;
 export function halfvecType(dimensions: any): any;
+export function bitType(dimensions: any): any;
 export function sparsevecType(dimensions: any): any;
