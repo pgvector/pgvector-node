9ee0180d23b7f256527fc019953bd4ff47830dcb
Andrew Kane <andrew@ankane.org>
2024-06-02 14:11:57 -0700

    Improved test [skip ci]

--- tests/utils/sparse-vector.test.mjs
+++ tests/utils/sparse-vector.test.mjs
@@ -2,4 +2,5 @@ import { SparseVector } from 'pgvector/utils';
 
 test('fromSql', () => {
   expect(SparseVector.fromSql('{1:1,3:2,5:3}/6').toArray()).toStrictEqual([1, 0, 2, 0, 3, 0]);
+  expect(SparseVector.fromSql('{1:1,3:2,5:3}/6').toString()).toStrictEqual('{1:1,3:2,5:3}/6');
 });
