0bf857c035ea357c1fef1a0a9c15429e8bcb295d
Andrew Kane <andrew@ankane.org>
2024-06-02 14:13:43 -0700

    Added test for fromDense [skip ci]

--- tests/utils/sparse-vector.test.mjs
+++ tests/utils/sparse-vector.test.mjs
@@ -2,5 +2,8 @@ import { SparseVector } from 'pgvector/utils';
 
 test('fromSql', () => {
   expect(SparseVector.fromSql('{1:1,3:2,5:3}/6').toArray()).toStrictEqual([1, 0, 2, 0, 3, 0]);
-  expect(SparseVector.fromSql('{1:1,3:2,5:3}/6').toString()).toStrictEqual('{1:1,3:2,5:3}/6');
+});
+
+test('fromDense', () => {
+  expect(SparseVector.fromDense([1, 0, 2, 0, 3, 0]).toString()).toStrictEqual('{1:1,3:2,5:3}/6');
 });
