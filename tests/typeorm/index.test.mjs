2a763e4fbbe384ad324f369498dbc03c4db8cb6e
Andrew Kane <andrew@ankane.org>
2024-06-26 09:21:33 -0700

    Added more tests for TypeORM

--- tests/typeorm/index.test.mjs
+++ tests/typeorm/index.test.mjs
@@ -1,4 +1,5 @@
 import pgvector from 'pgvector';
+import { SparseVector } from 'pgvector';
 import { DataSource, EntitySchema } from 'typeorm';
 
 test('example', async () => {
@@ -13,9 +14,18 @@ test('example', async () => {
         primary: true,
         generated: true
       },
+      // custom types not supported
+      // https://github.com/typeorm/typeorm/issues/10056
       embedding: {
-        // vector type not supported
-        // https://github.com/typeorm/typeorm/issues/10056
+        type: String
+      },
+      half_embedding: {
+        type: String
+      },
+      binary_embedding: {
+        type: String
+      },
+      sparse_embedding: {
         type: String
       }
     }
@@ -31,12 +41,12 @@ test('example', async () => {
 
   await AppDataSource.query('CREATE EXTENSION IF NOT EXISTS vector');
   await AppDataSource.query('DROP TABLE IF EXISTS typeorm_items');
-  await AppDataSource.query('CREATE TABLE typeorm_items (id bigserial PRIMARY KEY, embedding vector(3))');
+  await AppDataSource.query('CREATE TABLE typeorm_items (id bigserial PRIMARY KEY, embedding vector(3), half_embedding halfvec(3), binary_embedding bit(3), sparse_embedding sparsevec(3))');
 
   const itemRepository = AppDataSource.getRepository(Item);
-  await itemRepository.save({embedding: pgvector.toSql([1, 1, 1])});
-  await itemRepository.save({embedding: pgvector.toSql([2, 2, 2])});
-  await itemRepository.save({embedding: pgvector.toSql([1, 1, 2])});
+  await itemRepository.save({embedding: pgvector.toSql([1, 1, 1]), half_embedding: pgvector.toSql([1, 1, 1]), binary_embedding: '000', sparse_embedding: SparseVector.fromDense([1, 1, 1]).toSql()});
+  await itemRepository.save({embedding: pgvector.toSql([2, 2, 2]), half_embedding: pgvector.toSql([2, 2, 2]), binary_embedding: '101', sparse_embedding: SparseVector.fromDense([2, 2, 2]).toSql()});
+  await itemRepository.save({embedding: pgvector.toSql([1, 1, 2]), half_embedding: pgvector.toSql([1, 1, 2]), binary_embedding: '111', sparse_embedding: SparseVector.fromDense([1, 1, 2]).toSql()});
 
   const items = await itemRepository
     .createQueryBuilder('item')
@@ -48,6 +58,15 @@ test('example', async () => {
   expect(pgvector.fromSql(items[0].embedding)).toStrictEqual([1, 1, 1]);
   expect(pgvector.fromSql(items[1].embedding)).toStrictEqual([1, 1, 2]);
   expect(pgvector.fromSql(items[2].embedding)).toStrictEqual([2, 2, 2]);
+  expect(pgvector.fromSql(items[0].half_embedding)).toStrictEqual([1, 1, 1]);
+  expect(pgvector.fromSql(items[1].half_embedding)).toStrictEqual([1, 1, 2]);
+  expect(pgvector.fromSql(items[2].half_embedding)).toStrictEqual([2, 2, 2]);
+  expect(items[0].binary_embedding).toStrictEqual('000');
+  expect(items[1].binary_embedding).toStrictEqual('111');
+  expect(items[2].binary_embedding).toStrictEqual('101');
+  expect(SparseVector.fromSql(items[0].sparse_embedding).toArray()).toStrictEqual([1, 1, 1]);
+  expect(SparseVector.fromSql(items[1].sparse_embedding).toArray()).toStrictEqual([1, 1, 2]);
+  expect(SparseVector.fromSql(items[2].sparse_embedding).toArray()).toStrictEqual([2, 2, 2]);
 
   await AppDataSource.destroy();
 });
