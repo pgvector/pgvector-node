a84cedcee557700ed970f1b19403ac5f95051f98
Andrew Kane <andrew@ankane.org>
2024-06-26 09:27:37 -0700

    Added more tests for Slonik

--- tests/slonik/index.test.mjs
+++ tests/slonik/index.test.mjs
@@ -1,4 +1,5 @@
 import pgvector from 'pgvector';
+import { SparseVector } from 'pgvector';
 import { createPool, sql } from 'slonik';
 
 test('example', async () => {
@@ -6,12 +7,21 @@ test('example', async () => {
 
   await pool.query(sql.unsafe`CREATE EXTENSION IF NOT EXISTS vector`);
   await pool.query(sql.unsafe`DROP TABLE IF EXISTS slonik_items`);
-  await pool.query(sql.unsafe`CREATE TABLE slonik_items (id serial PRIMARY KEY, embedding vector(3))`);
+  await pool.query(sql.unsafe`CREATE TABLE slonik_items (id serial PRIMARY KEY, embedding vector(3), half_embedding halfvec(3), binary_embedding bit(3), sparse_embedding sparsevec(3))`);
 
   const embedding1 = pgvector.toSql([1, 1, 1]);
   const embedding2 = pgvector.toSql([2, 2, 2]);
   const embedding3 = pgvector.toSql([1, 1, 2]);
-  await pool.query(sql.unsafe`INSERT INTO slonik_items (embedding) VALUES (${embedding1}), (${embedding2}), (${embedding3})`);
+  const halfEmbedding1 = pgvector.toSql([1, 1, 1]);
+  const halfEmbedding2 = pgvector.toSql([2, 2, 2]);
+  const halfEmbedding3 = pgvector.toSql([1, 1, 2]);
+  const binaryEmbedding1 = '000';
+  const binaryEmbedding2 = '101';
+  const binaryEmbedding3 = '111';
+  const sparseEmbedding1 = SparseVector.fromDense([1, 1, 1]).toSql();
+  const sparseEmbedding2 = SparseVector.fromDense([2, 2, 2]).toSql();
+  const sparseEmbedding3 = SparseVector.fromDense([1, 1, 2]).toSql();
+  await pool.query(sql.unsafe`INSERT INTO slonik_items (embedding, half_embedding, binary_embedding, sparse_embedding) VALUES (${embedding1}, ${halfEmbedding1}, ${binaryEmbedding1}, ${sparseEmbedding1}), (${embedding2}, ${halfEmbedding2}, ${binaryEmbedding2}, ${sparseEmbedding2}), (${embedding3}, ${halfEmbedding3}, ${binaryEmbedding3}, ${sparseEmbedding3})`);
 
   const embedding = pgvector.toSql([1, 1, 1]);
   const items = await pool.query(sql.unsafe`SELECT * FROM slonik_items ORDER BY embedding <-> ${embedding} LIMIT 5`);
@@ -19,6 +29,15 @@ test('example', async () => {
   expect(pgvector.fromSql(items.rows[0].embedding)).toStrictEqual([1, 1, 1]);
   expect(pgvector.fromSql(items.rows[1].embedding)).toStrictEqual([1, 1, 2]);
   expect(pgvector.fromSql(items.rows[2].embedding)).toStrictEqual([2, 2, 2]);
+  expect(pgvector.fromSql(items.rows[0].half_embedding)).toStrictEqual([1, 1, 1]);
+  expect(pgvector.fromSql(items.rows[1].half_embedding)).toStrictEqual([1, 1, 2]);
+  expect(pgvector.fromSql(items.rows[2].half_embedding)).toStrictEqual([2, 2, 2]);
+  expect(items.rows[0].binary_embedding).toStrictEqual('000');
+  expect(items.rows[1].binary_embedding).toStrictEqual('111');
+  expect(items.rows[2].binary_embedding).toStrictEqual('101');
+  expect(SparseVector.fromSql(items.rows[0].sparse_embedding).toArray()).toStrictEqual([1, 1, 1]);
+  expect(SparseVector.fromSql(items.rows[1].sparse_embedding).toArray()).toStrictEqual([1, 1, 2]);
+  expect(SparseVector.fromSql(items.rows[2].sparse_embedding).toArray()).toStrictEqual([2, 2, 2]);
 
   await pool.query(sql.unsafe`CREATE INDEX ON slonik_items USING hnsw (embedding vector_l2_ops)`);
 
