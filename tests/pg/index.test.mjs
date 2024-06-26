de04da9ba70a56e0c6cc6602ec72f56b0e95e744
Andrew Kane <andrew@ankane.org>
2024-06-26 08:16:51 -0700

    Added test for bit type for node-postgres [skip ci]

--- tests/pg/index.test.mjs
+++ tests/pg/index.test.mjs
@@ -9,15 +9,15 @@ test('example', async () => {
   await pgvector.registerType(client);
 
   await client.query('DROP TABLE IF EXISTS pg_items');
-  await client.query('CREATE TABLE pg_items (id serial PRIMARY KEY, embedding vector(3), half_embedding halfvec(3))');
+  await client.query('CREATE TABLE pg_items (id serial PRIMARY KEY, embedding vector(3), half_embedding halfvec(3), binary_embedding bit(3))');
 
   const params = [
-    pgvector.toSql([1, 1, 1]), pgvector.toSql([1, 1, 1]),
-    pgvector.toSql([2, 2, 2]), pgvector.toSql([2, 2, 2]),
-    pgvector.toSql([1, 1, 2]), pgvector.toSql([1, 1, 2]),
-    null, null
+    pgvector.toSql([1, 1, 1]), pgvector.toSql([1, 1, 1]), '000',
+    pgvector.toSql([2, 2, 2]), pgvector.toSql([2, 2, 2]), '101',
+    pgvector.toSql([1, 1, 2]), pgvector.toSql([1, 1, 2]), '111',
+    null, null, null
   ];
-  await client.query('INSERT INTO pg_items (embedding, half_embedding) VALUES ($1, $2), ($3, $4), ($5, $6), ($7, $8)', params);
+  await client.query('INSERT INTO pg_items (embedding, half_embedding, binary_embedding) VALUES ($1, $2, $3), ($4, $5, $6), ($7, $8, $9), ($10, $11, $12)', params);
 
   const { rows } = await client.query('SELECT * FROM pg_items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 1, 1])]);
   expect(rows.map(v => v.id)).toStrictEqual([1, 3, 2, 4]);
@@ -27,6 +27,9 @@ test('example', async () => {
   expect(rows[0].half_embedding).toStrictEqual([1, 1, 1]);
   expect(rows[1].half_embedding).toStrictEqual([1, 1, 2]);
   expect(rows[2].half_embedding).toStrictEqual([2, 2, 2]);
+  expect(rows[0].binary_embedding).toStrictEqual('000');
+  expect(rows[1].binary_embedding).toStrictEqual('111');
+  expect(rows[2].binary_embedding).toStrictEqual('101');
 
   await client.query('CREATE INDEX ON pg_items USING hnsw (embedding vector_l2_ops)');
 
