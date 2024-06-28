a51fc65faef44e447acf048626d7356434968349
Andrew Kane <andrew@ankane.org>
2024-06-27 17:05:02 -0700

    Updated types [skip ci]

--- types/sequelize/index.d.ts
+++ types/sequelize/index.d.ts
@@ -1,4 +1,5 @@
 export function registerType(Sequelize: any): void;
+export function registerTypes(Sequelize: any): void;
 export function l2Distance(column: any, value: any, sequelize: any): any;
 export function maxInnerProduct(column: any, value: any, sequelize: any): any;
 export function cosineDistance(column: any, value: any, sequelize: any): any;
