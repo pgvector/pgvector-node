de352021155dcd7865f63da844121c1bb9a7ba70
Andrew Kane <andrew@ankane.org>
2026-05-28 09:56:50 -0700

    Updated packages

--- tests/mikro-orm.test.mjs
+++ tests/mikro-orm.test.mjs
@@ -27,8 +27,7 @@ test('mikro-orm example', async () => {
   await em.execute('CREATE EXTENSION IF NOT EXISTS vector');
   await em.execute('DROP TABLE IF EXISTS mikro_items');
 
-  const generator = orm.getSchemaGenerator();
-  await generator.createSchema();
+  await orm.schema.create();
 
   em.create(Item, {embedding: [1, 1, 1], half_embedding: [1, 1, 1], binary_embedding: '000', sparse_embedding: new SparseVector([1, 1, 1])});
   em.create(Item, {embedding: [2, 2, 2], half_embedding: [2, 2, 2], binary_embedding: '101', sparse_embedding: new SparseVector([2, 2, 2])});
