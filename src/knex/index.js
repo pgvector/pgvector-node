9c3ed4125f16f13de89d9d84aedd936349fdab94
Andrew Kane <andrew@ankane.org>
2026-05-28 11:05:19 -0700

    Removed enableExtension for Knex.js and Objection.js [skip ci]

--- src/knex/index.js
+++ src/knex/index.js
@@ -1,10 +1,6 @@
 import knex from 'knex';
 import { fromSql, toSql, vectorType, halfvecType, sparsevecType } from '../utils/index.js';
 
-knex.SchemaBuilder.extend('enableExtension', function (name) {
-  return this.raw('CREATE EXTENSION IF NOT EXISTS ??', [name]);
-});
-
 knex.TableBuilder.extend('vector', function (name, options) {
   const dimensions = options && (Number.isInteger(options) ? options : options.dimensions);
   return this.specificType(name, vectorType(dimensions));
