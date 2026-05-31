f98a37311494632a54c46f30b29ee9dd37591784
Andrew Kane <andrew@ankane.org>
2026-05-30 18:14:45 -0700

    Improved types [skip ci]

--- src/kysely/index.js
+++ src/kysely/index.js
@@ -1,28 +1,50 @@
-// @ts-nocheck
-
 import { sql } from 'kysely';
 import { fromSql, toSql } from '../index.js';
 
+/**
+ * @param {any} column
+ * @param {any} value
+ */
 export function l2Distance(column, value) {
   return sql`${sql.ref(column)} <-> ${toSql(value)}`;
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ */
 export function maxInnerProduct(column, value) {
   return sql`${sql.ref(column)} <#> ${toSql(value)}`;
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ */
 export function cosineDistance(column, value) {
   return sql`${sql.ref(column)} <=> ${toSql(value)}`;
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ */
 export function l1Distance(column, value) {
   return sql`${sql.ref(column)} <+> ${toSql(value)}`;
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ */
 export function hammingDistance(column, value) {
   return sql`${sql.ref(column)} <~> ${value}`;
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ */
 export function jaccardDistance(column, value) {
   return sql`${sql.ref(column)} <%> ${value}`;
 }
--- src/mikro-orm/index.js
+++ src/mikro-orm/index.js
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import { raw } from '@mikro-orm/core';
 import { toSql } from '../index.js';
 
@@ -8,6 +6,13 @@ export { HalfvecType } from './halfvec.js';
 export { SparsevecType } from './sparsevec.js';
 export { VectorType } from './vector.js';
 
+/**
+ * @param {string} op
+ * @param {any} column
+ * @param {any} value
+ * @param {any} em
+ * @param {boolean} [binary]
+ */
 function distance(op, column, value, em, binary) {
   if (raw) {
     return raw(`?? ${op} ?`, [column, binary ? value : toSql(value)]);
@@ -16,26 +21,56 @@ function distance(op, column, value, em, binary) {
   }
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ * @param {any} em
+ */
 export function l2Distance(column, value, em) {
   return distance('<->', column, value, em);
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ * @param {any} em
+ */
 export function maxInnerProduct(column, value, em) {
   return distance('<#>', column, value, em);
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ * @param {any} em
+ */
 export function cosineDistance(column, value, em) {
   return distance('<=>', column, value, em);
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ * @param {any} em
+ */
 export function l1Distance(column, value, em) {
   return distance('<+>', column, value, em);
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ * @param {any} em
+ */
 export function hammingDistance(column, value, em) {
   return distance('<~>', column, value, em, true);
 }
 
+/**
+ * @param {any} column
+ * @param {any} value
+ * @param {any} em
+ */
 export function jaccardDistance(column, value, em) {
   return distance('<%>', column, value, em, true);
 }
