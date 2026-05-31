e6c709169875726a5de5027b55076609ea190357
Andrew Kane <andrew@ankane.org>
2026-05-30 23:45:52 -0700

    Updated style [skip ci]

--- src/knex/index.d.ts
+++ src/knex/index.d.ts
@@ -1,11 +1,11 @@
 import 'knex';
 
 declare module 'knex' {
-    namespace Knex {
-        interface TableBuilder {
-            vector(name: string, options: any): Knex.TableBuilder;
-            halfvec(name: string, options: any): Knex.TableBuilder;
-            sparsevec(name: string, options: any): Knex.TableBuilder;
-        }
+  namespace Knex {
+    interface TableBuilder {
+      vector(name: string, options: any): Knex.TableBuilder;
+      halfvec(name: string, options: any): Knex.TableBuilder;
+      sparsevec(name: string, options: any): Knex.TableBuilder;
     }
+  }
 }
