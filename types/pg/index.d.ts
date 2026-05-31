597072d24ac949de97b521a7f1ccfb4d7cac5487
Andrew Kane <andrew@ankane.org>
2026-05-30 17:57:08 -0700

    Fixed type [skip ci]

--- types/pg/index.d.ts
+++ types/pg/index.d.ts
@@ -4,7 +4,7 @@ declare namespace _default {
     export { toSql };
 }
 export default _default;
-export function registerType(client: Client): Promise<void>;
-export function registerTypes(client: Client): Promise<void>;
+export function registerType(client: any): Promise<void>;
+export function registerTypes(client: any): Promise<void>;
 import { toSql } from '../index.js';
 export { toSql };
