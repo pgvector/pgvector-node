f09a8e9d7c6bc157b815fb7bb80d486c2c0c282f
Andrew Kane <andrew@ankane.org>
2026-06-09 18:57:31 -0700

    Fixed CI

--- tests/pg-native.test.js
+++ tests/pg-native.test.js
@@ -1,10 +1,12 @@
 import assert from 'node:assert';
 import test from 'node:test';
+// @ts-ignore
 import Client from 'pg-native';
 import pgvector from 'pgvector/pg';
 import { SparseVector } from 'pgvector';
 
 function isDeno() {
+  // @ts-ignore
   return typeof Deno !== 'undefined';
 }
 
@@ -25,11 +27,11 @@ test('pg-native example', {skip: isDeno()}, () => {
   client.querySync('INSERT INTO pg_native_items (embedding, half_embedding, binary_embedding, sparse_embedding) VALUES ($1, $2, $3, $4), ($5, $6, $7, $8), ($9, $10, $11, $12), ($13, $14, $15, $16)', params);
 
   const rows = client.querySync('SELECT * FROM pg_native_items ORDER BY embedding <-> $1 LIMIT 5', [pgvector.toSql([1, 1, 1])]);
-  assert.deepEqual(rows.map(v => v.id), [1, 3, 2, 4]);
+  assert.deepEqual(rows.map(/** @type {function(any): number} */ v => v.id), [1, 3, 2, 4]);
   assert.deepEqual(pgvector.fromSql(rows[0].embedding), [1, 1, 1]);
   assert.deepEqual(pgvector.fromSql(rows[0].half_embedding), [1, 1, 1]);
   assert.deepEqual(rows[0].binary_embedding, '000');
-  assert.deepEqual(pgvector.fromSql(rows[0].sparse_embedding).toArray(), [1, 1, 1]);
+  assert.deepEqual(pgvector.fromSql(rows[0].sparse_embedding), new SparseVector([1, 1, 1]));
 
   client.querySync('CREATE INDEX ON pg_native_items USING hnsw (embedding vector_l2_ops)');
 
